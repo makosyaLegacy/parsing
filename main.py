2abfe2a2d0ec56b52086108306777d47eb36fc53
Павлов Святослав <pandapo.mako@gmail.com>
2026-04-22 22:40:20 +0500

    Добавил цикл для характеристики, буду фиксить дубликаты

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import re
 
 import aiohttp
 from bs4 import BeautifulSoup as BS
@@ -15,9 +16,19 @@ async def main():
             soup = BS(html, 'html.parser')
             name = soup.find("h1", {"class": "Typography ProductInfoMobileSmall_title__6HplU Typography__XL"})
             price = soup.find("p", {"class": "Typography ProductPricesVariantB_accented__n2rtH Typography__Heading Typography__Heading_H1"})
+            rating = soup.find("p", {"class": "Typography RatingAndReviewsCount_rating__evIGS Typography__Caption Typography__Caption_Bold"})
+
+            specs = soup.find_all("div", class_=re.compile(r"Description_item"))
+            for spec in specs:
+                key = spec.find("p", class_=re.compile(r"Description_leftText"))
+                value = spec.find("p", class_=re.compile(r"Description_rightText"))
+                if key and value:
+                    print(f"{key.get_text(strip=True)}: {value.get_text(strip=True)}")
 
             print(f"Название: {name.get_text(strip=True)}")
             print(f"Цена: {price.get_text(strip=True)}")
+            print(f"Рейтинг: {rating.get_text(strip=True)}")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
